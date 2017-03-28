d11314e4a63ce95d82c316f19297e2480d7a23a5
47912513V <47912513V@A26O04.novaulas.stucom.com>
2017-03-28 19:52:51 +0200

    diferents menus

--- web/index.php
+++ web/index.php
@@ -9,10 +9,14 @@
         <script src="../libs/Jquery/jquery-3.2.0.min.js" type="text/javascript"></script>
         <script src="../libs/angular_basic/angular.min.js" type="text/javascript"></script>
         <script src="../libs/angular_basic/angular-resource.min.js" type="text/javascript"></script>
-        <link href="../libs/bootstrap/css/bootstrap-theme.min.css" rel="stylesheet" type="text/css"/>
+        <link href="../libs/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
         <script src="../libs/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
     </head>
     <body>
+        <div class="container">
+        <h1 align="center">SINTESIS UF3<small>Magner & Torrafa</small></h1>
+        </div>
+        <button class="btn btn-primary">iueqg</button>
         <?php
 
         ?>
