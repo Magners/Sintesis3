f1a769b8bf7a12d0ab993110463d9d2d77961d41
47912513V <47912513V@A26O04.novaulas.stucom.com>
2017-03-28 19:20:25 +0200

    libs

---
+++ web/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <script src="../libs/Jquery/jquery-3.2.0.min.js" type="text/javascript"></script>
+        <script src="../libs/angular_basic/angular.min.js" type="text/javascript"></script>
+        <script src="../libs/angular_basic/angular-resource.min.js" type="text/javascript"></script>
+        <link href="../libs/bootstrap/css/bootstrap-theme.min.css" rel="stylesheet" type="text/css"/>
+        <script src="../libs/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+    </head>
+    <body>
+        <?php
+
+        ?>
+    </body>
+</html>
