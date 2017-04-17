6e041cc322e02769362fd1ff2d852c9964a24944
Ferran Torredeflot <ftorredeflot@gmail.com>
2017-04-17 18:04:36 +0200

    preparando estructura angular

--- web/indexAngular.php
+++ web/indexAngular.php
@@ -1,24 +1,48 @@
 <!DOCTYPE html>
-<!--
-
--->
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
-        <script src="../libs/Jquery/jquery-3.2.0.min.js" type="text/javascript"></script>
+        <title>Angular</title>
+        <link href="../libs/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <script src="../js/newjavascript.js" type="text/javascript"></script>
         <script src="../libs/angular_basic/angular.min.js" type="text/javascript"></script>
         <script src="../libs/angular_basic/angular-resource.min.js" type="text/javascript"></script>
-        <link href="../libs/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-        <script src="../libs/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
-        <link href="../testing.css" rel="stylesheet" type="text/css"/>
+        
     </head>
     <body>
-        <div align="center">
-        <h1>ANGULAR</h1>
-        <?php
-        ?>
-        <button class="btn btn-primary" onclick="window.location='index.php'">Retroceder</button>
+        <div class="container">
+            <div class="row">
+                <div class="page-header">
+                    <h1 align="center">Angular</h1>
+                </div>
+            </div>
+
+            <div class="row">
+
+
+                <button  type="button" class="btn btn-block btn-info" id="start">Begin</button>
+
+            </div>
+            <div class="row">
+                <div class="col-lg-8">
+                    <div id="image" height="100" width="100">
+
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div id="clue">
+
+                    </div>
+                    <div id="question">
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="row">
+                <hr>
+                <a class="btn btn-primary" href='index.php'>Retroceder</a>
+            </div>
         </div>
     </body>
 </html>
