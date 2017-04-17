3df0b35708951d1a9fab1837fb3bfad329c90b8e
Ferran Torredeflot <ftorredeflot@gmail.com>
2017-04-17 17:15:16 +0200

    frontend js

--- web/indexJs.php
+++ web/indexJs.php
@@ -2,19 +2,44 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
-        <script src="../libs/Jquery/jquery-3.2.0.min.js" type="text/javascript"></script>
-        <script src="../libs/angular_basic/angular.min.js" type="text/javascript"></script>
-        <script src="../libs/angular_basic/angular-resource.min.js" type="text/javascript"></script>
+        <title>JAVACRIPT PLANO</title>
         <link href="../libs/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-        <script src="../libs/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
-        <link href="../testing.css" rel="stylesheet" type="text/css"/>
+        <script src="../js/newjavascript.js" type="text/javascript"></script>
     </head>
     <body>
-        <div align="center">
-        <h1>JAVACRIPT PLANO</h1>
-        
-        <a class="btn btn-primary" href='index.php'>Retroceder</a>
+        <div class="container">
+            <div class="row">
+                <div class="page-header">
+                    <h1 align="center">JAVACRIPT PLANO</h1>
+                </div>
+            </div>
+
+            <div class="row">
+
+
+                <button  type="button" class="btn btn-block btn-info" id="start">Begin</button>
+
+            </div>
+            <div class="row">
+                <div class="col-lg-8">
+                    <div id="image" height="100" width="100">
+
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div id="clue">
+
+                    </div>
+                    <div id="question">
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="row">
+                <hr>
+                <a class="btn btn-primary" href='index.php'>Retroceder</a>
+            </div>
         </div>
     </body>
 </html>
