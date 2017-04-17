5965299c317db33c5d79e149fd1fbbfc1934a803
Ferran Torredeflot <ftorredeflot@gmail.com>
2017-04-17 19:37:39 +0200

    canviat a json encode

--- web/serv.php
+++ web/serv.php
@@ -1,25 +1,19 @@
 <?php
 session_start();
 @header("Content-type: application/json");
- $json = "";
+
 if (isset($_GET['inicio']) && $_GET['inicio']=='si') {     
-    $_SESSION['ruta'] = "img/505.jpg";
-   
-    $json .='{"ruta":"'.$_SESSION['ruta'].'",';//retornamos el numero generado
+    $_SESSION['ruta'] = array("ruta"=>"img/505.jpg");
+    $json= json_encode($_SESSION['ruta']);
 } 
 if(isset($_GET['pista']) && $_GET['pista']=='si'){
-    $_SESSION['pista'] = "compleix les regles aritmetiques...";
-     $json .='{"pista":"'.$_SESSION['pista'].'",';//retornamos el numero generado
+     $_SESSION['pista'] = array("pista"=>"compleix les regles aritmetiques...");
+     $json= json_encode($_SESSION['pista']);
 }
 if(isset($_GET['respuestas']) && $_GET['respuestas']=='si'){
-    $_SESSION['respuestas'] = array(18,25);
-    
-     $json .='{"good":'.$_SESSION['respuestas'][0].",".
-             '"bad":'.$_SESSION['respuestas'][1].',';//retornamos el numero generado
+    $_SESSION['respuestas'] = array("good"=>18,"bad"=>25);  
+     $json = json_encode($_SESSION['respuestas']);
 }
-//finalizamos la estructura XML
-$json .= '"":""}';
-//insertamos la respuesta XML
 echo($json);
 
 
